a3479a27ce107fbf928a4bf76d30db2cc86dac05
Emmanuella <emmanuella.chukwu77@gmail.com>
2023-08-21 19:57:02 +0100

    experimental genre file / minor content

---
+++ src/utils/genres.ts
@@ -0,0 +1,20 @@
+import { Genre } from "../../typings";
+
+export const MovieGenre: Array<Genre> = [
+  { id: 28, name: "Action" },
+  { id: 35, name: "Comedy" },
+  { id: 10749, name: "Romance" },
+  { id: 18, name: "Drama" },
+  { id: 16, name: "Animation" },
+  { id: 12, name: "Advemture" },
+  { id: 27, name: "Horror" },
+];
+
+export const SeriesGenre: Array<Genre> = [
+  { id: 99, name: "Documentary" },
+  { id: 10759, name: "Action" },
+  { id: 10764, name: "Reality" },
+  { id: 10762, name: "Kids" },
+  { id: 10751, name: "Family" },
+  { id: 9648, name: "Mystery" },
+];
